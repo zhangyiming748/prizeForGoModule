769acad26d1f98e148742b35872747b343445c9e
Zen <zhangyiming748@gmail.com>
2020-11-07 21:04:50 +0800

    修改为有输出条目

--- doLottery/draw.go
+++ doLottery/draw.go
@@ -1,56 +1,58 @@
 package doLottery
 
 import (
+	"fmt"
+	"sync"
+
+	"github.com/zhangyiming748/prizeForGoModule/allToSlice"
 	"github.com/zhangyiming748/prizeForGoModule/getRandom"
 	"github.com/zhangyiming748/prizeForGoModule/member"
-	"github.com/zhangyiming748/prizeForGoModule/allToSlice"
-	"sync"
 )
 
 func ChooseOne(s string) string {
-	pl:=member.Set2Map(s)
+	pl := member.Set2Map(s)
 
-	sl:=allToSlice.MakeSlice(pl)
+	sl := allToSlice.MakeSlice(pl)
 	var wg sync.WaitGroup
-	c1:=make(chan string,1)
-	c2:=make(chan string,1)
-	c3:=make(chan string,1)
+	c1 := make(chan string, 1)
+	c2 := make(chan string, 1)
+	c3 := make(chan string, 1)
 	wg.Add(1)
 	go func() {
 		defer func() {
 			wg.Done()
 		}()
-		n:=getRandom.GetNum(len(sl))
-		v :=sl[n]
-		c1<-v
+		n := getRandom.GetNum(len(sl))
+		v := sl[n]
+		c1 <- v
 	}()
 	wg.Add(1)
 	go func() {
 		defer func() {
 			wg.Done()
 		}()
-		n:=getRandom.GetNum(len(sl))
-		v :=sl[n]
-		c2<-v
+		n := getRandom.GetNum(len(sl))
+		v := sl[n]
+		c2 <- v
 	}()
 	wg.Add(1)
 	go func() {
 		defer func() {
 			wg.Done()
 		}()
-		n:=getRandom.GetNum(len(sl))
-		v :=sl[n]
-		c3<-v
+		n := getRandom.GetNum(len(sl))
+		v := sl[n]
+		c3 <- v
 	}()
 	select {
 	case ret := <-c1:
-		//fmt.Printf("天选之子是: %s\t天选之子是: %s\t天选之子是: %s\n", ret, ret, ret)
+		fmt.Printf("中奖的这个逼是: %s,大家一起来鄙视他\n", ret)
 		return ret
 	case ret := <-c2:
-		//fmt.Printf("天选之子是: %s\t天选之子是: %s\t天选之子是: %s\n", ret, ret, ret)
+		fmt.Printf("中奖的这个逼是: %s,大家一起来鄙视他\n", ret)
 		return ret
 	case ret := <-c3:
-		//fmt.Printf("天选之子是: %s\t天选之子是: %s\t天选之子是: %s\n", ret, ret, ret)
+		fmt.Printf("中奖的这个逼是: %s,大家一起来鄙视他\n", ret)
 		return ret
 	}
 }
