c7a390a4d99bbf0d29c8059dea67618a90f26f5f
Zen <zhangyiming748@gmail.com>
2020-11-07 21:06:34 +0800

    取消输出中间结果

--- member/setMember.go
+++ member/setMember.go
@@ -2,38 +2,38 @@ package member
 
 import (
 	"bufio"
+	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
 func Set2Map(s string) map[string]int {
-	m:=make(map[string]int)
-	f,err:=os.Open(s)
-	if err!=nil{
-		log.Printf("读取文件发生错误,报错信息是: %v", err)
+	m := make(map[string]int)
+	f, err := os.Open(s)
+	if err != nil {
+		fmt.Printf("读取文件发生错误,报错信息是: %v", err)
 	}
 	defer func() {
 		_ = f.Close()
 	}()
-	br :=bufio.NewReader(f)
-	for{
-		a,_,c:=br.ReadLine()
+	br := bufio.NewReader(f)
+	for {
+		a, _, c := br.ReadLine()
 		//a=[]uint8
-		if c == io.EOF{
-			log.Println("录入结束")
+		if c == io.EOF {
+			//log.Println("录入结束")
 			break
 		}
-		result:=uint8ToString(a)
-		if _,ok:=m[result];ok{
-			m[result]=m[result]+1
-		}else {
-			m[result]=1
+		result := uint8ToString(a)
+		if _, ok := m[result]; ok {
+			m[result] = m[result] + 1
+		} else {
+			m[result] = 1
 		}
 	}
 	return m
 }
 func uint8ToString(u8 []uint8) string {
-	strout:=string(u8)
+	strout := string(u8)
 	return strout
 }
